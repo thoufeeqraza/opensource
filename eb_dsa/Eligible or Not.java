13f3844ac85c3540e8a5b9542839a7b893dd98f1
thoufeeqraza <thoufeeqraza9654@gmail.com>
2024-11-12 15:48:51 +0530

    Create Eligible or Not.java

---
+++ eb_dsa/Eligible or Not.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N>=6 && N<=8){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        
+}
+}
