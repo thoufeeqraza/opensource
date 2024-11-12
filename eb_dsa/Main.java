6ff017110fcf73ad6e8e89f6b8114204a52d1418
thoufeeqraza <thoufeeqraza9654@gmail.com>
2024-11-12 10:35:44 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,8 @@
+ class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
