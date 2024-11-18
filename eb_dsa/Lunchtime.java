25678216d14bb867af75c5476c2d2f52d6ae7a5b
thoufeeqraza <thoufeeqraza9654@gmail.com>
2024-11-18 21:12:38 +0530

    Create Lunchtime.java

---
+++ eb_dsa/Lunchtime.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>=1 && x<=4){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
